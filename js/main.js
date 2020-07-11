6423994ef4d14be7ef67b8185763f1391b7d577a
Gladys Cruz <cruzgladysazucena@johndeere.com>
2020-07-11 01:59:43 -0500

    adding Tower of hanoi image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log("scrit linked");
+
+
+
+let disksArray = document.querySelectorAll(".disk-item");
+
+disksArray.forEach(disk => {
+    // let diskArray = disk;
+    console.log(disk.id);
+    // diskArray.forEach(diskItem => {
+    //     console.log(diskItem);
+    // });
+
+});
